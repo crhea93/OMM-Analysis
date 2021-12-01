de5fe9f1841622948bb54d30032161cc66a13488
Carter Rhea <carterrhea93@gmail.com>
2021-12-01 13:25:04 -0500

    minor changes to final_alignment.ipynb

--- example.i
+++ example.i
@@ -1,8 +1,8 @@
-home_dir = /home/carterrhea/Desktop/200815
+home_dir = /media/carterrhea/carterrhea/OMM/
 dome_dir = DomeFlat
 target_dir = Target/NGC6946
 filter_ = Ha'
 pos_dil = _pos
-output_dir = /home/carterrhea/Desktop/NGC6946/Ha
-num_pos = 1
+output_dir = /home/carterrhea/Desktop
+num_pos = 2
 api_key = vyihgprfjfltyhvv
--- reductionPipeline.py
+++ reductionPipeline.py
@@ -1,11 +1,14 @@
 import sys
+import os
 from sort_filters import sort_filters
 from stack_and_clean import stack_and_clean
 from mosaic import mosaic
 
+current_dir = os.getcwd()
 # Sort by filter
 sort_filters(sys.argv[1])
 # Stack and clean the images. Solve for astrometry
 stack_and_clean(sys.argv[1])
 # Create mosaic
+os.chdir(current_dir)
 mosaic(sys.argv[1])
