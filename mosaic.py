26a4ffe5042b32ac64f9cc02c9de532b877234de
carterrhea <carterrhea93@gmail.com>
2022-07-14 13:49:56 -0400

    fixed mosaic

--- mosaic.py
+++ mosaic.py
@@ -11,43 +11,40 @@ from reproject.mosaicking import reproject_and_coadd
 from reproject.mosaicking import find_optimal_celestial_wcs
 #----------------------------INPUTS-------------------------------#
 
-'''sci_dir = "/home/carterrhea/Documents/"
-name = 'Arp94'
-sci_dir = sci_dir + name
-output_dir = sci_dir'''
 #-----------------------------------------------------------------#
 def mosaic(sys_arg):
-	inputs = read_input_file(sys_arg)
-	#output_dir = sci_dir
-	name = inputs['target_dir'].split('/')[-1]
-	sci_dir = inputs['output_dir']#+'/'+name+'/'+inputs['filter_']
+    os.chdir('../..')
+    inputs = read_input_file(sys_arg)
+    name = inputs['target_dir'].split('/')[-1]
+    sci_dir = inputs['output_dir']#+'/'+name+'/'+inputs['filter_']
+    sci_hdus = []  # List of science images
+    for filename in os.listdir(sci_dir):
+        if filename.endswith('.fits') and filename.startswith('stacked_correct'):
+            hdu = fits.open(sci_dir+'/'+filename)
+            sci_hdus.append(hdu)
+    if len(sci_hdus) > 1:
+        print("#-----Calculaing WCS-----#")
+        wcs_out, shape_out = find_optimal_celestial_wcs(sci_hdus, auto_rotate=True)
+        print("#-----Reprojecting-----#")
+        array, footprint = reproject_and_coadd(sci_hdus,
+            wcs_out, shape_out=shape_out,
+            reproject_function=reproject_interp,
+            match_background=True,
+            combine_function='mean')
+        print("#-----Making Image-----#")
+        plt.figure(figsize=(10, 8))
+        ax1 = plt.subplot(1, 2, 1)
+        im1 = ax1.imshow(np.log10(array), origin='lower', vmin=np.max(np.log10(array))-2, vmax=np.max(np.log10(array)))
+        ax1.set_title('Mosaic')
+        ax2 = plt.subplot(1, 2, 2)
+        im2 = ax2.imshow(footprint, origin='lower')
+        ax2.set_title('Footprint')
+        plt.savefig(sci_dir+'/mosaic.png')
+        print('#-----Making Fits-----#')
+        hdu = fits.PrimaryHDU(header=wcs_out.to_header(), data=array)
+        hdul = fits.HDUList([hdu])
+        hdul.writeto(sci_dir+'/'+name+'.fits', overwrite=True)
+    else:
+        print("There is only one image so there is no need to make a mosaic")
 
-	sci_hdus = []  # List of science images
-	for filename in os.listdir(sci_dir):
-		if filename.endswith('.fits') and filename.startswith('stacked_correct'):
-			hdu = fits.open(sci_dir+'/'+filename)
-			sci_hdus.append(hdu)
-
-	print(sci_hdus)
-	print("#-----Calculaing WCS-----#")
-	wcs_out, shape_out = find_optimal_celestial_wcs(sci_hdus, auto_rotate=True)
-	print("#-----Reprojecting-----#")
-	array, footprint = reproject_and_coadd(sci_hdus,
-		                               wcs_out, shape_out=shape_out,
-		                               reproject_function=reproject_interp,
-		                               match_background=True,
-		                               combine_function='mean')
-	print("#-----Making Image-----#")
-	plt.figure(figsize=(10, 8))
-	ax1 = plt.subplot(1, 2, 1)
-	im1 = ax1.imshow(np.log(array), origin='lower', vmin=np.max(np.log(array))-4, vmax=np.max(np.log(array)))
-	ax1.set_title('Mosaic')
-	ax2 = plt.subplot(1, 2, 2)
-	im2 = ax2.imshow(footprint, origin='lower')
-	ax2.set_title('Footprint')
-	plt.savefig(sci_dir+'/mosaic.png')
-
-	print('#-----Making Fits-----#')
-	hdu = fits.PrimaryHDU(header=wcs_out.to_header(), data=array)
-	hdul = fits.HDUList([hdu])
-	hdul.writeto(sci_dir+'/'+name+'.fits', overwrite=True)
+    
